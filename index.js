9d8987c41d8df76d9e46a5156d253d509ad7664d
Carlos Grijalva <grijalva.carlos@gmail.com>
2021-10-27 12:17:21 -0400

    Add get request

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const cookieParser = require("cookie-parser");
 const app = express();
 const PORT = 3000;
 
+app.get("/", (req, res) => {
+  res.send("hellow world");
+});
+
 app.listen(PORT, () => {
   console.log(`server is listening on port: ${PORT}`);
 });
