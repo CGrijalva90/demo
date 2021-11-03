362e0fc7838aa6a85500d126584f90d3d7d7fc9a
Carlos Grijalva <grijalva.carlos@gmail.com>
2021-11-03 15:28:45 -0400

    Update helper.js file

--- helpers.js
+++ helpers.js
@@ -1,5 +1,7 @@
-// Helpers function below
+const express = require("express");
+const cookieParser = require("cookie-parser");
 
+// Helpers function below
 const logErrors = function (err, req, res, next) {
   if (err) {
     console.error(`Error: ${err.message}\r\nStack: ${err.stack}`);
