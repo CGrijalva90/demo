4723591898175186f7895283ba3cbda59d19bcd5
Carlos Grijalva <grijalva.carlos@gmail.com>
2021-12-13 15:46:10 -0600

    Updated the index.js file with a new endpoint

--- index.js
+++ index.js
@@ -5,8 +5,12 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-  res.send("hellow world");
+  res.send("hello world");
 });
+
+app.get("/home", (req, res) => {
+  res.send("Greetings from the home page");
+})
 
 app.listen(PORT, () => {
   console.log(`server is listening on port: ${PORT}`);
